f833e2a595ecb06f30665c2208764eaa37038b0b
Carlos Julian H. Chiongbian <carlos.chiongbian@lanexus.com>
2023-03-26 22:43:17 +0800

    feat: added static messages for dev

---
+++ src/constant/static.ts
@@ -0,0 +1,17 @@
+export const STATIC_MESSAGES = [
+  {
+    fromOthers: false,
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultrices tristique lectus, nec tincidunt ligula pharetra at. Donec fringilla consequat condimentum. Cras auctor varius elementum. Vestibulum ultricies nunc vel sapien sagittis, quis ullamcorper justo sagittis. Nullam euismod ut lectus vel pellentesque. Aenean eu tempor mi. In lobortis ornare ligula, in mattis mauris convallis sit amet. Sed lacinia placerat nulla, at ullamcorper ex lobortis id.",
+  },
+  {
+    fromOthers: true,
+    content:
+      "Lorem ipsum dolor sit lobortis ornare ligula, in mattis mauris convallis sit amet. Sed lacinia placerat nulla, at ullamcorper ex lobortis id.",
+  },
+  {
+    fromOthers: false,
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultrices tristique.",
+  },
+];
